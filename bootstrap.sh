3a6def4e46fa53e1caedfb6ee663c311f5de66ea
Jens W. Klein <jk@kleinundpartner.at>
2018-10-01 11:10:11 +0200

    fix, thx frisi

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,11 @@ git clone -b 5.2 git@github.com:plone/buildout.coredev.git
 # for py 2.7
 cd py2
 virtualenv -p /usr/bin/python2.7 --clear .
-./bin/pip install -r requirements.txt
+./bin/pip install -r ../buildout.coredev/requirements.txt
 ./bin/buildout -N
 
 # for py3.6
 cd ../py3
 python3.6 -m venv .
-./bin/pip install -r requirements.txt
+./bin/pip install -r ../buildout.coredev/requirements.txt
 ./bin/buildout -N
