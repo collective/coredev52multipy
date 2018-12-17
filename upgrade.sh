7bf48b73f7eda57520b7c72e641bd0e50c4ad26b
Jens W. Klein <jk@kleinundpartner.at>
2018-12-17 10:01:02 +0100

    convinience script upgrading both

---
+++ upgrade.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Install one checkout of buildout.coredev for Python 2 and 3
+# This script uses pyenv to install separate virtualenvs
+TAG=5.2
+PY2=2.7.15
+PY3=3.7.1
+VENVBASE=plone-coredev-$TAG
+
+cd buildout.coredev
+git pull --rebase --autostash
+cd ..
+
+eval "$(pyenv init -)"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+eval "$(pyenv virtualenv-init -)"
+
+
+upgrade() {
+    VENV=$VENVBASE-$1
+    echo "---------------------------------------------------------------------------"
+    echo "Install Python $2 into directory $1 using venv $VENV"
+    echo "---------------------------------------------------------------------------"
+    cd $1
+    pyenv activate $VENV
+    pip install -r ../buildout.coredev/requirements.txt
+    buildout -N
+    pyenv deactivate
+    cd ..
+}
+
+upgrade py2 
+upgrade py3 
+
